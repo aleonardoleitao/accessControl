7af14db5aa9db7f399ca2b780ea362781b26abf6
Andre Leitao <andrel@horizonte.tv.br>
2015-08-09 12:38:44 -0300

    alterando para exibicao no IOS

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -15,32 +15,72 @@ class VideoController < ApplicationController
   end
 
   def exibe_video
-    Rails.logger.info "Iniciando a consulta no exibe video"
-    Rails.logger.info params
 
-    token = params[:token] + "&e=" + params[:e]
-    video = Video.find_by_token(token)
-    tk = params[:tk]
-    perfil = params[:perfil]
-    resultado = 1
-    xml = Nokogiri::XML(open('http://ws.conecte.us/index.asp?id=' + perfil + '&acao=auth_mp4&token=' + URI::encode(tk)))
-    itens = xml.search('status').map do |item|
-      resultado = item.text
-    end
-
-    Rails.logger.info "Resultado da consulta"
-    if !video.status && !(resultado == '1')
-      video.status = true
-      video.save
-      respond_to do |format|
-        #format.mp4 { send_file File.join(["/mnt/Vids", params[:caminho1], params[:caminho2] + ".mp4"]), :type => 'video/mp4', :disposition => :inline, :stream => true, :buffer_size  =>  4096 }
-        format.mp4 { send_file File.join(["/mnt/Vids", params[:caminho1], params[:caminho2] + ".mp4"]), :type => 'video/mp4', :buffer_size  =>  1024 }
-        #format.mp4 { send_file File.join(["/mnt/Vids/3000/1000.mp4"]), :type => 'video/mp4', :buffer_size  =>  1024 }
+      user_agent = request.env['HTTP_USER_AGENT']
+      
+      chrome = false
+      if user_agent =~ /Chrome/
+        chrome = true
+        Rails.logger.info "Chrome"
       end
-    else
-        render(:file => "#{Rails.root}/public/403.html", :status => 403, :layout => false)
-    end
 
+      file_path = File.join(["/mnt/Vids", params[:caminho1], params[:caminho2] + ".mp4"])
+      file_name = params[:caminho2] + ".mp4"
+
+      if(request.headers["HTTP_RANGE"]) && !chrome
+
+        size = File.size(file_path)
+        bytes = Rack::Utils.byte_ranges(request.headers, size)[0]
+        offset = bytes.begin
+        length = bytes.end  - bytes.begin
+
+        #response.header["Accept-Ranges"]=  "bytes"
+        #response.header["Content-Range"] = "bytes #{bytes.begin}-#{bytes.end}/#{size}"
+        
+        Rails.logger.info "bytes #{bytes.begin}-#{bytes.end}/#{size}"
+
+        Rails.logger.info "Iniciando o envio"
+
+        send_data IO.binread(file_path,length, offset), :type => "video/mp4", :stream => true,  :disposition => 'inline', :file_name => file_name
+
+        Rails.logger.info "Arquivo enviado"
+      else
+        Rails.logger.info "Exibindo videos do chrome"
+        Rails.logger.info file_path
+        respond_to do |format|
+          format.mp4 { send_file(file_path, :disposition => 'inline', :stream => true, :file_name => file_name, :type => 'video/mp4', :buffer_size  =>  1024 )}
+        end
+        Rails.logger.info "Finalizando videos do chrome"
+      end
   end
+
+  #def exibe_video
+    #Rails.logger.info "Iniciando a consulta no exibe video"
+    #Rails.logger.info params
+
+    #token = params[:token] + "&e=" + params[:e]
+    #video = Video.find_by_token(token)
+    #tk = params[:tk]
+    #perfil = params[:perfil]
+    #resultado = 1
+    #xml = Nokogiri::XML(open('http://ws.conecte.us/index.asp?id=' + perfil + '&acao=auth_mp4&token=' + URI::encode(tk)))
+    #itens = xml.search('status').map do |item|
+    #  resultado = item.text
+    #end
+
+    #Rails.logger.info "Resultado da consulta"
+    #if !video.status && !(resultado == '1')
+    #  video.status = true
+    #  video.save
+  #    respond_to do |format|
+  #      #format.mp4 { send_file File.join(["/mnt/Vids", params[:caminho1], params[:caminho2] + ".mp4"]), :type => 'video/mp4', :disposition => :inline, :stream => true, :buffer_size  =>  4096 }
+  #      format.mp4 { send_file File.join(["/mnt/Vids", params[:caminho1], params[:caminho2] + ".mp4"]), :type => 'video/mp4', :buffer_size  =>  1024 }
+  #      #format.mp4 { send_file File.join(["/mnt/Vids/3000/1000.mp4"]), :type => 'video/mp4', :buffer_size  =>  1024 }
+  #    end
+    #else
+    #    render(:file => "#{Rails.root}/public/403.html", :status => 403, :layout => false)
+    #end
+
+  #end
 
 end
