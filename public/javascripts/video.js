8a23348cc73a9ba7c307a59b34c516b8b7dd209f
Andre <andrel@MacBook-Pro-de-Novas.local>
2015-09-14 23:04:48 -0300

    configuracao

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -83,7 +83,7 @@ function exibeVideo(urlServer, imagem, urlCompleta, idVideo, width, perfil) {
 		poster: imagem,
 		source: urlCompleta,
 		parentId: idVideo,
-		persistConfig: false,
+		width: "100%",
 		//width: width,
 		autoPlay: true,
 		hideMediaControl: false,
