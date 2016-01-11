1eaf0a48a1972c01dc7d52a61b720d3830ea74e8
Andre <aleonardo.leitao@gmail.com>
2016-01-11 21:26:48 -0200

    correcao para windows phone

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -167,7 +167,7 @@ class VideoController < ApplicationController
 
     mobile_android =  "palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile|zune"
     mobile_iphone =  "ipod|ipad|iphone"
-    mobile_windowsce = "windows ce"
+    mobile_windowsce = "windows ce|wmfsdk"
 
     mobile_android = Regexp.new(mobile_android).match(user_agent.to_s.downcase)
     mobile_iphone = Regexp.new(mobile_iphone).match(user_agent.to_s.downcase)
@@ -183,6 +183,7 @@ class VideoController < ApplicationController
       mobile_windowsce = 0 
     end
 
+    Rails.logger.info ("UserAgente - #{user_agent} ")
     Rails.logger.info ("Android - #{mobile_android}")
     Rails.logger.info ("IOs - #{mobile_iphone}")
     Rails.logger.info ("Windows CE - #{mobile_windowsce}")
@@ -246,6 +247,9 @@ class VideoController < ApplicationController
     end
 
     file_name = params[:caminho2] + ".mp4"
+    if file_name == "4125945F1D0BE26B4474C897026704D1B88E621082015073455" || file_name == "41259E2B5E7A778474B62AA8464709CBA962B20082015120530" || file_name == "412595829D51A8AC549F8B3FB7E67C41238F217082015163546"
+      diretorio = "/opt/railsapps/videos"
+    end
     file_path = File.join([diretorio, params[:caminho1], params[:caminho2] + ".mp4"])
     
 
@@ -261,7 +265,6 @@ class VideoController < ApplicationController
     #itens = xml.search('status').map do |item|
     #  resultado = item.text
     #end
-    resultado = 0
 
     Rails.logger.info "Resultado da consulta - webserver - #{resultado}"
     Rails.logger.info "Resultado da consulta - video token - #{video.status}"
@@ -290,15 +293,17 @@ class VideoController < ApplicationController
     Rails.logger.info ("Android - #{mobile_android}")
     Rails.logger.info ("IOs - #{mobile_iphone}")
     Rails.logger.info ("Windows CE - #{mobile_windowsce}")
+    Rails.logger.info ("Range - #{request.headers['HTTP_RANGE']} ")
 
-    if !(resultado == '1') && ((mobile_android!=0 || mobile_iphone!=0 || mobile_windowsce!=0) || (!video.status))
+    if resultado && ((mobile_android!=0 || mobile_windowsce!=0) || request.headers['HTTP_RANGE'])
+    #if !(resultado == '1') && ((mobile_android!=0 || mobile_iphone!=0 || mobile_windowsce!=0) || (!video.status))
 
       video.status = true
       video.save
 
-      Rails.logger.info "Exbindo o video"
+      Rails.logger.info "Exbindo o video - streaming"
 
-      if (mobile_iphone!=0) || firefox || ((request.headers["HTTP_RANGE"]) && !chrome && !(mobile_android != 0))
+      if (mobile_iphone!=0) || ((request.headers["HTTP_RANGE"]) && !chrome && !firefox && !(mobile_android != 0))
 
         size = File.size(file_path)
         bytes = Rack::Utils.byte_ranges(request.headers, size)[0]
@@ -307,23 +312,18 @@ class VideoController < ApplicationController
 
         response.header["Accept-Ranges"]=  "bytes"
         response.header["Content-Range"] = "bytes #{bytes.begin}-#{bytes.end}/#{size}"
-        #response.header["Content-Length"] = size
 
         Rails.logger.info "bytes #{bytes.begin}-#{bytes.end}/#{size}"
         Rails.logger.info "Iniciando o envio IOS"
         send_data IO.binread(file_path,length, offset), :type => "video/mp4", :stream => true, :x_sendfile => true, :disposition => 'inline', :file_name => file_name, :buffer_size  =>  2048
-        #x_accel_redirect IO.binread(file_path,length, offset), :type => "video/mp4", :stream => true,  :disposition => 'inline', :file_name => file_name
         Rails.logger.info "Arquivo enviado IOS"
 
       else
-
-        Rails.logger.info "Exibindo videos apenas para o chrome"
+        Rails.logger.info "Exibindo videos apenas para o chrome e firefox"
         Rails.logger.info file_path
         respond_to do |format|
-          format.mp4 { send_file(file_path, :file_name => file_name, :stream => true, :x_sendfile => true, :type => 'video/mp4')}
-          #format.mp4 { x_accel_redirect("/protected/41259/4125945F1D0BE26B4474C897026704D1B88E621082015073455.mp4", :disposition => 'inline', :stream => true, :file_name => file_name, :type => 'video/mp4', :buffer_size  =>  2048 )}
+          format.mp4 { send_file(file_path, :disposition => 'inline', :stream => true, :file_name => file_name, :type => 'video/mp4')}
         end
-
         Rails.logger.info "Finalizando videos do chrome"
       end
 
